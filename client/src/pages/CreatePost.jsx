0165a1117763eaf8eb6586ce2ca0606dd187fde2
Raghav <iraghav.me@gmail.com>
2023-04-25 22:35:42 -0400

    Updated post request url for image dalle route

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -19,7 +19,7 @@ const CreatePost = () => {
     if(form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://imaginate.onrender.com/api/v1/post', {
+        const response = await fetch('https://imaginate.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
